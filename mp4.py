6f0786a709cbd63ac9d513282889875bee2205f0
senura11 <111191247+senura11@users.noreply.github.com>
2022-08-30 19:08:38 +0530

    Add files via upload

---
+++ mp4.py
@@ -0,0 +1,25 @@
+def mp4():
+    import time,pytube,os,warnings
+    warnings.filterwarnings("ignore")
+    text=input('\033[1;35;40menter youtube link or text : ')
+    time.sleep(0.2)
+    try:
+        search=pytube.Search(text)
+        watch_url=search.results[0].watch_url
+    except:
+        print('\033[1;31;40mcheck your internet connection and try again...')
+        exit()
+    mp4=pytube.YouTube(watch_url)
+    dmp4=mp4.streams.get_audio_only('mp4')
+    os.chdir('/storage/emulated/0/')#path
+    if 'yt-music' in os.listdir():#path
+        None
+    else:
+        os.mkdir('yt-music')#path
+    print('\033[1;31;40mdownloading your audio - '+str(mp4.title))
+    path='/storage/emulated/0/yt-music'#path
+    dmp4.download(path)
+    print('your audio download complete...')
+    print('your audio saved ',path)
+if __name__=='__main__':
+    mp4()
