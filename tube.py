f621e4b8d785e1501712e77fcc400e8106352373
senupama <111191247+senura11@users.noreply.github.com>
2022-08-31 14:45:02 +0530

    Update tube.py
    update.......

--- tube.py
+++ tube.py
@@ -25,6 +25,7 @@ print('\033[1;32;40m'+str(banner)+'\033[1;34;40m')
 print('       Tool by dark devil     ')
 print('\n\n')
 print('\033[1;33;40m[1] \033[1;31;40mstart                      \033[1;33;40m[2] \033[1;31;40mupdate tool')
+print('\033[1;33;40m[3] \033[1;31;40mcontact us                 \033[1;33;40m[4] \033[1;31;40mExit')
 print()
 choice=input('\033[1;34;40menter your choice : ')
 if choice=='1' or choice=='start':
@@ -34,17 +35,23 @@ if choice=='1' or choice=='start':
     except:
         os.system('pip install pytube')
         import pytube
+    print('\n\n\n\n')
     print('\033[1;33;40m[1] \033[1;31;40mmp4                    \033[1;33;40m[2] \033[1;31;40mvideo') 
     print()
-    choice=input('\033[1;34;40menter your choice : ')
+    choice2=input('\033[1;34;40menter your choice : ')
     os.system('clear')#os command
     #mp4 download
-    if choice=='1' or choice=='mp4':
+    if choice2=='1' or choice2=='mp4':
         mp4.mp4()
         
      #video download
-    elif choice=='2' or choice=='video':
+    elif choice2=='2' or choice2=='video':
         video.video()
+elif choice=='3' or choice=='contact us':
+      os.system('clear')
+      os.system('termux-open https://wa.me/+94787576427?text=hey')
+elif choice=='4' or choice=='Exit':
+    exit()
 
         
         
